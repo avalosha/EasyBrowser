ad605ab6129f60cb6a27e7c4fb430f4a05591c8c
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2019-12-07 15:20:22 -0600

    Add UITableView to choose the initial website.

--- EasyBrowser/EasyBrowser/ViewController.swift
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -6,97 +6,42 @@
 //  Copyright © 2019 Álvaro Ávalos Hernández. All rights reserved.
 //
 
-import WebKit
+import UIKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-
-    var webView: WKWebView!
-    var progressView: UIProgressView!
-    var websites = ["google.com", "apple.com", "hackingwithswift.com"]
-    
-    override func loadView(){
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
+class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //Se añade un observador para detectar cuando exista un cambio con estimatedProgress
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-        
-        //Instancia y agrega un UIProgressView como UIBarButtonItem
-        progressView = UIProgressView(progressViewStyle: .default)
-        progressView.sizeToFit()
-        //Añadir UIBarButtonItems con UIToolbar
-        let progressButton = UIBarButtonItem(customView: progressView)
-        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let back = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
-        let forward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-
-        //Configura la matriz de toolbaritems
-        toolbarItems = [progressButton, spacer, back, forward, refresh]
-        navigationController?.isToolbarHidden = false
-        
-        let url = URL(string: "https://" + websites[0])!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        title = "Web Browser"
     }
     
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "estimatedProgress" {
-            progressView.progress = Float(webView.estimatedProgress)
-        }
+    //MARK - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websites.count
     }
 
-    @objc func openTapped() {
-        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
-        for website in websites {
-            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Website", for: indexPath)
+        cell.textLabel?.text = websites[indexPath.row]
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "goToWeb", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! WebViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.index = indexPath.row
         }
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
-        present(ac, animated: true)
     }
-    
-    func openPage(action: UIAlertAction) {
-        let url = URL(string: "https://" + action.title!)!
-        webView.load(URLRequest(url: url))
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
-    }
-    
-    //Verifica la URL
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        let url = navigationAction.request.url
-        //Desenvuelve si existe el host de la pagina
-        if let host = url?.host {
-            for website in websites {
-                if host.contains(website) {
-                    //Se permite la carga
-                    decisionHandler(.allow)
-                    print("entro")
-                    return
-                }
-            }
-        }
-        //Cancela la carga
-        decisionHandler(.cancel)
-        print("no entro")
-        showAlert()
-    }
-    
-    //Muestra alerta por no ser una URL reconocida
-    func showAlert() {
-        let ac = UIAlertController(title: "Wrong! That URL isn't valid.", message: nil, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        present(ac, animated: true)
-    }
+
 }
 
---
+++ EasyBrowser/EasyBrowser/Websites.swift
@@ -0,0 +1,11 @@
+//
+//  Websites.swift
+//  EasyBrowser
+//
+//  Created by Álvaro Ávalos Hernández on 07/12/19.
+//  Copyright © 2019 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import Foundation
+
+var websites = ["google.com", "apple.com", "hackingwithswift.com"]
