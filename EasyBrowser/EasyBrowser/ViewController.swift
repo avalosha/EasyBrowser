87833dd61e2c8378df1b975dece3f4186a0c9a8b
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2019-12-06 21:22:46 -0600

    Refactor code and check proper URL.

--- EasyBrowser/EasyBrowser/ViewController.swift
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView(){
         webView = WKWebView()
@@ -39,7 +40,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         toolbarItems = [progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
@@ -52,8 +53,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -66,6 +68,23 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    //Verifica la URL
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        //Desenvuelve si existe el host de la pagina
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    //Se permite la carga
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        //Cancela la carga
+        decisionHandler(.cancel)
     }
 }
 
